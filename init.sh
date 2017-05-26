11c6c481a6a931b589c6d4592b2f943ab65b8e8a
GeJusdot <tianmg@tianmgdeMacBook-Pro.local>
2017-05-26 15:20:14 +0800

    delete test files

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ source ~/.bash_profile
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
-pip install ping
+pip  install ping
 pip install tornado
 pip install futures
 pip install supervisor
